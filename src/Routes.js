31867c06d8d4c5157009eecd3e3b69ff5b179b8a
Pedro Barros <pedrobarros0101@outlook.com>
2022-03-26 13:51:24 -0300

    [FEAT][HOMEPAGE]
    iniciando projeto do front, construindo pastas para telas, imagens e componentes e criando arquivo de rotas do site

---
+++ src/Routes.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./views/HomePage";
+
+
+
+const Rotas = () => {
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<HomePage />}/>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+export default Rotas
---
+++ src/views/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Golden from '../assets/golden-logo.svg'
+
+
+const HomePage = () => {
+    return(
+        <>
+        <p>Ola Mundo</p>
+        <img src={Golden} alt="Logo do Golden"/>
+        </>
+    );
+}
+export default HomePage;
